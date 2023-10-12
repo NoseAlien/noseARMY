ef4f9897683a88e470f205e525a22e6fd373c5f1
NoseAlien <k021g1117@g.neec.ac.jp>
2023-10-12 17:00:21 +0900

    ADXComponent.hにコメントを執筆

--- ADXEngine/ADXComponent.h
+++ ADXEngine/ADXComponent.h
@@ -10,18 +10,34 @@ private:
 	ADXObject* gameObject = nullptr;
 
 public:
+	//更新処理
 	void Update(ADXObject* obj);
+	//Update()より前にgameObjectを代入する
 	void SetGameObject(ADXObject* obj);
+	/// <summary>
+	/// <para>アクセス違反を防ぐための更新処理を行う関数</para>
+	/// <para>消えそうなオブジェクトを参照していたらここで切っておけば安全</para>
+	/// </summary>
 	void SafetyPhase() {};
+	//gameObjectを取得
 	ADXObject* GetGameObject() const { return gameObject; };
 
 public:
+	//全オブジェクトで一斉に呼ばれる描画前処理
 	virtual void OnPreRender() {};
+	//gameObjectが描画される直前に呼ばれる描画前処理
 	virtual void OnWillRenderObject() {};
+	//gameObjectが描画された直後に呼ばれる描画後処理
 	virtual void Rendered() {};
+	//gameObjectが他のコライダーと接触した瞬間に呼ばれる処理
 	virtual void OnCollisionHit([[maybe_unused]] ADXCollider* col, [[maybe_unused]] ADXCollider* myCol) {};
 
 protected:
+	/// <summary>
+	/// <para>最初のUpdateが呼ばれたタイミングで呼ばれる</para>
+	/// <para>派生クラス固有の初期化処理</para>
+	/// </summary>
 	virtual void UniqueInitialize() {};
+	//派生クラス固有の更新処理
 	virtual void UniqueUpdate() {};
 };
